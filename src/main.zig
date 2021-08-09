ad1306fb2767838e0796d4a7befcb264686ad11c
Owen Lynch <root@owenlynch.org>
2021-08-09 12:56:48 -0400

    refactor to be more idiomatically ziggy

--- src/main.zig
+++ src/main.zig
@@ -4,64 +4,41 @@ const print = std.debug.print;
 const TypeInfo = std.builtin.TypeInfo;
 const Allocator = std.mem.Allocator;
 
-fn Dataframe(comptime n: usize, comptime fnames: [n]([]const u8), comptime Ts: [n]type) type {
-    comptime const cols_t: type = comptime blk: {
-        const nulfield: TypeInfo.StructField = .{
-            .name = "empty",
-            .field_type = i32,
-            .default_value = 0,
-            .is_comptime = true,
-            .alignment = 4
-        };
-        var fields: [n]TypeInfo.StructField = .{nulfield} ** n;
-        var i = 0;
-        inline while (i < n) : (i += 1) {
-            fields[i] = .{
-                .name = fnames[i],
-                .field_type = std.ArrayListUnmanaged(Ts[i]),
-                .default_value = null,
-                .is_comptime = false,
-                .alignment = @alignOf(std.ArrayListUnmanaged(Ts[i]))
-            };
-        }
-        const decls: [0]TypeInfo.Declaration = .{};
-        const s: TypeInfo.Struct = .{
-            .layout = TypeInfo.ContainerLayout.Auto,
-            .fields = &fields,
-            .decls = &decls,
-            .is_tuple = true,
-        };
-        break :blk @Type(TypeInfo { .Struct = s});
-    };
-    comptime const row_t: type = comptime blk: {
-        const nulfield: TypeInfo.StructField = .{
-            .name = "empty",
-            .field_type = i32,
-            .default_value = 0,
-            .is_comptime = true,
-            .alignment = 4
-        };
-        var fields: [n]TypeInfo.StructField = .{nulfield} ** n;
-        var i = 0;
-        inline while (i < n) : (i += 1) {
-            fields[i] = .{
+fn pi_type(comptime n: usize, comptime fnames: [n]([]const u8), comptime Ts: [n]type) type {
+    comptime const fields: [n]TypeInfo.StructField = comptime init: {
+        var tmp: [n]TypeInfo.StructField = undefined;
+        for (tmp) |*field, i| {
+            field.* = .{
                 .name = fnames[i],
                 .field_type = Ts[i],
                 .default_value = null,
                 .is_comptime = false,
-                .alignment = @alignOf(std.ArrayListUnmanaged(Ts[i]))
+                .alignment = @alignOf(Ts[i])
             };
         }
-        const decls: [0]TypeInfo.Declaration = .{};
-        const s: TypeInfo.Struct = .{
-            .layout = TypeInfo.ContainerLayout.Auto,
-            .fields = &fields,
-            .decls = &decls,
-            .is_tuple = true,
-        };
-        break :blk @Type(TypeInfo { .Struct = s});
+        break :init tmp;
+    };
+    comptime const decls: [0]TypeInfo.Declaration = .{};
+    comptime const s: TypeInfo.Struct = .{
+        .layout = TypeInfo.ContainerLayout.Auto,
+        .fields = &fields,
+        .decls = &decls,
+        .is_tuple = true,
+    };
+    return @Type(TypeInfo { .Struct = s});
+}
+
+fn Dataframe(comptime n: usize, comptime fnames: [n]([]const u8), comptime Ts: [n]type) type {
+    comptime const array_Ts: [n]type = comptime init: {
+        var tmp: [n]type = undefined;
+        inline for (tmp) |*T, i| {
+            T.* = std.ArrayListUnmanaged(Ts[i]);
+        }
+        break :init tmp;
     };
 
+    comptime const cols_t: type = pi_type(n, fnames, array_Ts);
+    comptime const row_t: type = pi_type(n, fnames, Ts);
 
     return struct {
         const Self = @This();
@@ -70,11 +47,14 @@ fn Dataframe(comptime n: usize, comptime fnames: [n]([]const u8), comptime Ts: [
         alloc: *Allocator,
 
         pub fn init(alloc: *Allocator, capacity: usize) !Self {
-            var cols: cols_t = undefined;
-            comptime var i = 0;
-            inline while(i < n) : (i += 1) {
-                cols[i] = try std.ArrayListUnmanaged(Ts[i]).initCapacity(alloc, capacity);
-            }
+            const cols: cols_t = init: {
+                var tmp: cols_t = undefined;
+                inline for (fnames) |_fname, i| {
+                    tmp[i] = try std.ArrayListUnmanaged(Ts[i]).initCapacity(alloc, capacity);
+                }
+                break :init tmp;
+            };
+
             return Self{
                 .cols = cols,
                 .len = 0,
@@ -83,18 +63,16 @@ fn Dataframe(comptime n: usize, comptime fnames: [n]([]const u8), comptime Ts: [
         }
 
         pub fn add_row(self: *Self, row: row_t) !void {
-            comptime var i = 0;
-            inline while(i < n) : (i += 1) {
+            inline for (fnames) |_fname, i| {
                 try self.cols[i].append(self.alloc, row[i]);
             }
             self.len += 1;
         }
 
         fn col2i(comptime col: []const u8) usize {
-            comptime var i = 0;
             comptime var found = false;
-            inline while (i < n) : (i += 1) {
-                if (std.mem.eql(u8, col, fnames[i])) {
+            inline for (fnames) |fname, i| {
+                if (std.mem.eql(u8, col, fname)) {
                     found = true;
                     return i;
                 }
@@ -116,27 +94,7 @@ fn Dataframe(comptime n: usize, comptime fnames: [n]([]const u8), comptime Ts: [
 }
     
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
-
 const Points = Dataframe(2, .{"x", "y"}, .{ []const u8, i32 });
-
-const LinkedList = struct {
-    pub const Node = struct {
-        prev: ?*Node,
-        next: ?*Node,
-        data: i32,
-    };
-
-    first: ?*Node,
-    last: ?*Node,
-    len: usize,
-};
 
 test "new points" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -146,5 +104,5 @@ test "new points" {
     try pts.add_row(.{ .x = "foo", .y = 3 });
     std.debug.assert(pts.len == 1);
     std.debug.assert(std.mem.eql(u8, try pts.get_col("x", 0), "foo"));
-    const y1 = try pts.get_col("y",1);
+    const y1 = try pts.get_col("y", 0);
 }
